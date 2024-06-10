a54411f06bf4de05cf706943f5786d155d984a36
Vadym Kovalchuk <106161570+vadym-kovalchuk-deltatre@users.noreply.github.com>
2024-06-10 20:56:59 +0300

    Rem comment

--- Router/src/pages/Error.jsx
+++ Router/src/pages/Error.jsx
@@ -3,7 +3,7 @@ import { MainNav } from "../components";
 export function ErrorPage() {
   return (
     <>
-      <MainNav />{" "}
+      <MainNav />
       <main>
         <h1>An error occurred!</h1>
         <p>Page not found!</p>
