f385226476171cc402ba75b33f86a66148268afe
Vadym Kovalchuk <106161570+vadym-kovalchuk-deltatre@users.noreply.github.com>
2024-07-16 15:50:51 +0300

    Prettier formater tailwindcss

--- Managing-tasks/src/App.jsx
+++ Managing-tasks/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <h1 className="my-8 bg-slate-400 text-center text-5xl font-bold text-gray-200">
+        Hello World
+      </h1>
     </>
   );
 }
